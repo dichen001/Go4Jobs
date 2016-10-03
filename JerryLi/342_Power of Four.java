6856d040fe2f6b8050ebae929d6443939f2b11f1
Jerry Shijie Li <sli41@ncsu.edu>
2016-10-02 23:45:38 -0400

    daily code notes

---
+++ JerryLi/342_Power of Four.java
@@ -0,0 +1,18 @@
+/*
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example:
+Given num = 16, return true. Given num = 5, return false.
+
+Follow up: Could you solve it without loops/recursion?
+*/
+
+ public boolean isPowerOfFour(int num) {
+        /*
+        Solution in Discusson:
+        detect bits on crucial position by AND &
+        */ 
+        return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
+        //0x55555555 is to get rid of those power of 2 but not power of 4
+        //so that the single 1 bit always appears at the odd position 
+    }
