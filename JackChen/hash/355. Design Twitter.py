32044fe43aa80e233bfb8b0dfda896a17328fe67
Di Chen <dchen20@ncsu.edu>
2016-10-12 01:48:11 -0400

    daily

---
+++ JackChen/hash/355. Design Twitter.py
@@ -0,0 +1,112 @@
+"""
+Design a simplified version of Twitter where users can post tweets, follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. Your design should support the following methods:
+
+postTweet(userId, tweetId): Compose a new tweet.
+getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+follow(followerId, followeeId): Follower follows a followee.
+unfollow(followerId, followeeId): Follower unfollows a followee.
+Example:
+
+Twitter twitter = new Twitter();
+
+// User 1 posts a new tweet (id = 5).
+twitter.postTweet(1, 5);
+
+// User 1's news feed should return a list with 1 tweet id -> [5].
+twitter.getNewsFeed(1);
+
+// User 1 follows user 2.
+twitter.follow(1, 2);
+
+// User 2 posts a new tweet (id = 6).
+twitter.postTweet(2, 6);
+
+// User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+// Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.getNewsFeed(1);
+
+// User 1 unfollows user 2.
+twitter.unfollow(1, 2);
+
+// User 1's news feed should return a list with 1 tweet id -> [5],
+// since user 1 is no longer following user 2.
+twitter.getNewsFeed(1);
+"""
+
+
+class Twitter(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.follows = {}
+        self.tweets = {}
+        self.counter = 1
+
+
+    def postTweet(self, userId, tweetId):
+        """
+        Compose a new tweet.
+        :type userId: int
+        :type tweetId: int
+        :rtype: void
+        """
+        if userId not in self.follows:
+            self.follows[userId] = [userId]
+        if userId not in self.tweets:
+            self.tweets[userId] = [(self.counter, tweetId)]
+        else:
+            self.tweets[userId].insert(0, (self.counter, tweetId))
+        self.counter += 1
+
+
+    def getNewsFeed(self, userId):
+        """
+        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+        :type userId: int
+        :rtype: List[int]
+        """
+        if userId in self.follows.keys():
+            feeds = [t for user in self.follows[userId] for t in self.tweets[user]]
+            feeds.sort(key= lambda t: t[0], reverse= True)
+            feeds = [c_f[1] for c_f in feeds]
+            return feeds[:10]
+        return []
+
+
+    def follow(self, followerId, followeeId):
+        """
+        Follower follows a followee. If the operation is invalid, it should be a no-op.
+        :type followerId: int
+        :type followeeId: int
+        :rtype: void
+        """
+        if followerId not in self.follows:
+            self.follows[followerId] = [followerId]
+            self.tweets[followerId] = []
+        if followeeId not in self.follows:
+            self.tweets[followeeId] = []
+        if followeeId not in self.follows[followerId]:
+            self.follows[followerId].append(followeeId)
+
+
+    def unfollow(self, followerId, followeeId):
+        """
+        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+        :type followerId: int
+        :type followeeId: int
+        :rtype: void
+        """
+        if followerId != followeeId and followerId in self.follows and followeeId in self.follows[followerId]:
+            self.follows[followerId].remove(followeeId)
+
+
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
