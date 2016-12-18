51cfd6569f59729427d7ac296e92d84557a90224
Di Chen <dchen20@ncsu.edu>
2016-12-18 17:57:16 -0500

    +

---
+++ JackChen/Google/294. Flip Game II.py
@@ -0,0 +1,41 @@
+"""
+You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+
+Write a function to determine if the starting player can guarantee a win.
+
+For example, given s = "++++", return true. The starting player can guarantee a win by flipping the middle "++" to become "+--+".
+
+Follow up:
+Derive your algorithm's runtime complexity.
+"""
+
+
+class Solution(object):
+    def canWin(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def win(pos):
+            for i in range(len(pos)-1):
+                if mem.get(pos):
+                    return mem[pos]
+                if pos[i] + 1 == pos[i+1]:
+                    tmp = pos[0:i] + pos[i+2:]
+                    next = win(tmp)
+                    if not next:
+                        mem[pos] = True
+                        return True
+                    else:
+                        mem[pos] = False
+            return False
+
+
+        n = len(s)
+        mem = {}
+        pos = (i for i in range(n) if s[i] == '+')
+        return win(pos)
+
+
+s = Solution()
+s.canWin("++++")
---
+++ JackChen/Google/341. Flatten Nested List Iterator.py
@@ -0,0 +1,82 @@
+"""
+Given a nested list of integers, implement an iterator to flatten it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+Given the list [[1,1],2,[1,1]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+Example 2:
+Given the list [1,[4,[6]]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+
+Show Company Tags
+Show Tags
+Show Similar Problems
+
+"""
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.stack = []
+        for i in range(len(nestedList))[::-1]:
+            self.stack.append(nestedList[i])
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop().getInteger()
+
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while self.stack:
+            cur = self.stack[-1]
+            if cur.isInteger():
+                return True
+            cur_list = self.stack.pop().getList()
+            for n in cur_list[::-1]:
+                self.stack.append(n)
+        return False
+
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
